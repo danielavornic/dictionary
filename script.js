9e64e9d54a2c45e3dd6653647134772fe1c1a8a8
Daniela <daniela.vornic@gmail.com>
2020-07-08 17:28:02 +0300

    style the posters in grid

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 $('document').ready(function() {
+
     $("#form").submit(function(event) {
         event.preventDefault();
         $('#results').empty();
-
+        $('#images').empty();
+        $('h2').remove();
+        var j = 0;
         var movie = $('#movie').val();
         var key = '2a2c8444adab25f6589b73bbc1372f9d';
+
         $.ajax({
             url: "https://api.themoviedb.org/3/search/movie?&query=" + movie,
             data: { 'api_key': key},
@@ -12,6 +16,8 @@ $('document').ready(function() {
             dataType: 'json',
             success: function(result) {
                 if (result.results.length > 0) {
+                    var message = '<h2>Posters for '+ movie +'</h2>';
+                    $('#results').before(message);
                     for (i in result.results) {
                         id = result.results[i].id;
                         $.ajax({
@@ -19,20 +25,43 @@ $('document').ready(function() {
                             contentType: "application/json",
                             dataType: 'json',
                             success: function(res) {
-                                console.log(res);
                                 for (x in res.posters) {
                                     var url = 'https://image.tmdb.org/t/p/w500/' + res.posters[x].file_path;
-                                    var img = '<img src="'+ url +'">';
+
+                                    var img = '<div class="poster">'+
+                                                '<img class="img-fluid" data-target="#carousel" data-slide-to="'+j+'" src="'+ url +'">'+
+                                                '</div>';
                                     $('#results').append(img);
+
+                                    var active = '';
+                                    if (j == 0) {
+                                        active = 'active'
+                                    }
+                                    var modal = '<div class="carousel-item '+ active +'">'+
+                                                    '<a href="'+ url +'" target="_blank" download>'+
+                                                    '<img class="d-block" src="'+ url +'"></a>'+
+                                                '</div>';
+                                    $('#images').append(modal);
+
+                                    j++;
                                 }
                             }
                         })
                     }
                 } else {
-                    var errorMessage = '<h2>No posters found :(</h2>';
-                    $('#results').append(errorMessage);
+                    var errorMessage = '<h2>No posters found for '+ movie +'</h2>';
+                    $('#results').before(errorMessage);
                 }
             }
         })
     })
+
+    $(document).bind('keyup', function(e) {
+        if (e.which == 39) {
+            $('.carousel').carousel('next');
+        }
+        else if (e.which == 37) {
+            $('.carousel').carousel('prev');
+        }
+    });
 })
